255fa18161477198445f4096bbc5877c4f6df008
rtabit <randytabit@gmail.com>
2022-11-18 22:06:53 -0500

    updates

---
+++ WebApplication-Logger-Test/WebApplication-Logger-Test/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication_Logger_Test.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
